a48af4a6b564c4d626bb6e703af948588050aa87
wbrum <wbrum93@gmail.com>
2016-03-24 01:46:22 -0300

    Adição de funcionalidade de extrair commits de um repositório e fix nos comentários

---
+++ extracao_commits/extracao_commits.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+import csv
+import github3
+
+git_username = 'wbrum'
+git_api_token = '1d031b21a0a9cd15803ef7f7b32e3627d0f666e1'
+git_repo = 'https://github.com/gems-uff/noworkflow'
+
+# csv name
+csv_name = "git_hub_commits.csv"
+
+
+def run_csv():
+    # Exporta commits de um repositório em formato CSV
+
+    output_csv = csv.writer(open(csv_name, 'wb'), delimiter=',')
+    github = github3.login(username=git_username, token=git_api_token)
+
+    # csv headers
+    headers = [
+        'sha',
+        'author',
+        'message',
+
+    ]
+
+    # escreve header rows
+    output_csv.writerow(headers)
+
+    # baixa issues e transcreve para csv
+    git_commits = github.repository("gems-uff", "noworkflow").iter_commits()
+    for git_issue in git_commits:
+        print git_issue.commit.message
+        commit = [
+            git_issue.sha,
+            git_issue.author,
+            git_issue.commit.message.encode('utf8'),
+
+        ]
+        output_csv.writerow(commit)
+
+
+if __name__ == '__main__':
+    run_csv()
--- extracao_issues_dsv/extracao_issues_dsv.py
+++ extracao_issues_dsv/extracao_issues_dsv.py
@@ -1,18 +1,19 @@
+# coding=utf-8
 import csv
 import github3
 
 git_username = 'wbrum'
-git_api_token = 'dde42796ee120eef93b50371b15df243d237d8d6'
+git_api_token = '1d031b21a0a9cd15803ef7f7b32e3627d0f666e1'
 git_repo = 'https://github.com/gems-uff/noworkflow'
-state = 'all' # 'open','closed' ou 'all'
+state = 'all'  # 'open','closed' ou 'all'
 
 # csv name
 csv_name = "git_hub_issues.csv"
 
+
 def run_csv():
-    """
-    Export github issues into a csv format
-    """
+    # Exporta issues de um repositório em formato CSV
+
     output_csv = csv.writer(open(csv_name, 'wb'), delimiter=',')
     github = github3.login(username=git_username, token=git_api_token)
 
@@ -27,34 +28,32 @@ def run_csv():
         'created_at',
         'updated_at',
         'closed_at',
-		'assignee',
+        'assignee',
     ]
 
     # escreve header rows
     output_csv.writerow(headers)
 
     # pega issues e transcreve para csv
-    git_issues = github.iter_repo_issues('gems-uff','noworkflow','',state)
+    git_issues = github.iter_repo_issues('gems-uff', 'noworkflow', '', state)
     for git_issue in git_issues:
         print git_issue.title
-     #   labels = ' '.join(git_issue.labels.color) #problema aqui
+        #   labels = ' '.join(git_issue.labels.color) #problema aqui
 
-        # alot of these are blank because they are not really 
-        # needed but if you need them just fill them out
-		#TODO
         issue = [
             git_issue.number,
             git_issue.title.encode('utf8'),
             git_issue.body.encode('utf8'),
-            git_issue.state, 
-            git_issue.user, 
+            git_issue.state,
+            git_issue.user,
             git_issue.labels,
             git_issue.created_at,
             git_issue.updated_at,
             git_issue.closed_at,
-			git_issue.assignee,
+            git_issue.assignee,
         ]
         output_csv.writerow(issue)
+
 
 if __name__ == '__main__':
     run_csv()
